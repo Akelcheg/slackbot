f31fb17effe2cb6e17f82172823ada135c16304d
akel <i.kalinin@conci.info>
2017-10-23 10:37:52 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var Bot = require('slackbots');
+
+// create a bot
+var settings = {
+    token: 'xoxb-259706768289-acmU2daEOln7BbPxyy30eI8P',
+    name: 'helpy'
+};
+
+var bot = new Bot(settings);
+
+bot.on('start', function () {
+    console.log("Bot started");
+});
+
+bot.on('message', function (data) {
+
+    if (data.type == "message" && data.username != "helpy") {
+
+        var user = data.channel;
+        var text = data.text;
+
+        bot.postMessage(user, 'Your message is "' + text + '"');
+    }
+});
